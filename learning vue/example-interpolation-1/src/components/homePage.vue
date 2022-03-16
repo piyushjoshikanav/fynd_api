a099e27d53e4957c4fe7ac52626143f64d8e6d1b
piyushjoshikanav <piyushjoshikanav@gmail.com>
2022-03-16 22:07:50 +0530

    vue part

---
+++ learning vue/example-interpolation-1/src/components/homePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    <h1>home component</h1>
+    <h2>print 2 using interpolation</h2>
+    <h3> addition of 2 numbers::{{10+20}}</h3>
+    <h3> subtraction of 2 numbers::{{100-20}}</h3>
+    <h3> multiplication  of 2 numbers::{{10*20}}</h3>
+    <h3> division of 2 numbers::{{100/20}}</h3>
+
+    </div>
+
+</template>
+<script>
+export default {};
+</script>
+<style  scoped>
+
+</style>
